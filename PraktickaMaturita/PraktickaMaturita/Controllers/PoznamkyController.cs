713e6083bf9992e1e24c683d73daf0619d83b7cd
Martin Šíl <martin.sil1337@gmail.com>
2022-04-01 09:35:52 +0200

    Nakousnutí přidávání poznámek

---
+++ PraktickaMaturita/PraktickaMaturita/Controllers/PoznamkyController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using PraktickaMaturita.Models;
+using PraktickaMaturita.Data;
+using System.Linq;
+
+namespace PraktickaMaturita.Controllers
+{
+    public class PoznamkyController : Controller
+    {
+        private ArchivPoznamekData _databaze;
+
+        public PoznamkyController(ArchivPoznamekData databaze)
+        {
+            _databaze = databaze;
+        }
+    }
+}
