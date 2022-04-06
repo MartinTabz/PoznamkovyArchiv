e06be475476fd3e17bb5b5bc89eafc7cee60363f
Martin Šíl <martin.sil1337@gmail.com>
2022-04-06 23:01:38 +0200

    Hotové přidávání poznámek jen pro každého uživatele

---
+++ PraktickaMaturita/PraktickaMaturita/Controllers/PoznamkyController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Session;
+using PraktickaMaturita.Data;
+using PraktickaMaturita.Models;
+
+namespace PraktickaMaturita.Controllers
+{
+    public class PoznamkyController : Controller
+    {
+        private ArchivPoznamekData _databaze;
+
+        public PoznamkyController(ArchivPoznamekData databaze)
+        {
+            _databaze = databaze;
+        }
+
+        [HttpGet]
+        public IActionResult Pridat()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Pridat(string nadpis, string popis)
+        {
+            if (nadpis == null || nadpis.Trim().Length == 0)
+                return RedirectToAction("Pridat");
+
+            Uzivatel? prihlasenyUzivatel = KdoJePrihlasen();
+
+            Poznamka pridavanaPoznamka = new Poznamka()
+            {
+                DatumVlozeni = DateTime.Now,
+                Nadpis = nadpis,
+                Popis = popis,
+                Autor = prihlasenyUzivatel,
+            };
+
+            _databaze.Add(pridavanaPoznamka);
+            _databaze.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Vypsat()
+        {
+            Uzivatel? prihlasenyUzivatel = KdoJePrihlasen();
+
+            if (prihlasenyUzivatel == null)
+                return RedirectToAction("Prihlasit", "Uzivatel");
+
+            List<Poznamka> vsechnyUkoly = _databaze.Poznamky
+                .Where(u => u.Autor == prihlasenyUzivatel)
+                .ToList();
+
+            return View(vsechnyUkoly);
+        }
+
+
+        private Uzivatel? KdoJePrihlasen()
+        {
+            string? jmenoPrihlasenehoUzivatele = HttpContext.Session.GetString("Prihlaseny");
+
+            if (jmenoPrihlasenehoUzivatele == null)
+                return null;
+
+            Uzivatel? prihlasenyUzivatel
+                = _databaze.Uzivatele
+                .Where(u => u.Jmeno == jmenoPrihlasenehoUzivatele)
+                .FirstOrDefault();
+
+            return prihlasenyUzivatel;
+        }
+    }
+}
---
+++ PraktickaMaturita/PraktickaMaturita/Migrations/20220406203711_DatumTed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PraktickaMaturita.Migrations
+{
+    public partial class DatumTed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Vlozeni",
+                table: "Poznamky",
+                newName: "DatumVlozeni");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DatumVlozeni",
+                table: "Poznamky",
+                newName: "Vlozeni");
+        }
+    }
+}
